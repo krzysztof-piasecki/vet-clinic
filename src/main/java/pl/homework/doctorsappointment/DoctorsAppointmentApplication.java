86a2a6bcfa3a74211714222004a7232b7aebd9b5
Krzysztof <krzysztof.piasecki1410@gmail.com>
2021-01-18 21:31:36 +0100

    Prepraring Java Project

---
+++ src/main/java/pl/homework/doctorsappointment/DoctorsAppointmentApplication.java
@@ -0,0 +1,13 @@
+package pl.homework.doctorsappointment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsAppointmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsAppointmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/homework/doctorsappointment/DoctorsAppointmentApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.homework.doctorsappointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsAppointmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
